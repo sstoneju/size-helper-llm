bc0470501fd5b4bddf4fc1e4b66fabec058b1ffb
Tyler <tyler@perfitt.io>
2024-01-25 18:31:56 +0900

    chore

---
+++ main.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import streamlit as st
+
+load_dotenv()
+
+
+llm = ChatOpenAI()
+output_parser = StrOutputParser()
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "너는 나이키 신발 중에서 일반 사이즈에 비해 크거나 작게나온 신발들을 평소신는 사이즈를 듣고 잘 맞는 사이즈를 찾아 줄 수 있는 사이즈 마스터야"),
+    ("user", "{input}")
+])
+
+chain = prompt | llm | output_parser
+
+st.title("나이키 핏 마스터")
+content = st.text_input('자주 신는 사이즈와 원하는 모델을 알려주세요.')
+
+if st.button('사이즈 알아보기' , type="primary"):
+    with st.spinner('사이즈를 알아보고 있어요...'):
+        result = chain.invoke({"input": f"{content}. 내가 어떤 사이즈를 신으면 잘 맞을까?"})
+        st.write(result)
